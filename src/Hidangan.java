eb97960f91d1537f8b198f9e96e4ee9e7fe66d09
LAB B-14 <kuwildansyah@gmail.com>
2023-03-06 18:05:31 +0700

    Latihan 1

---
+++ src/Hidangan.java
@@ -0,0 +1,12 @@
+public class Hidangan {
+    protected String namaHidangan;
+    public String getnamaHidangan(){
+        return namaHidangan;
+    }
+    public void setnamaHidangan(String namaHidangan){
+        this.namaHidangan = namaHidangan;
+    }
+    public String disantap(){
+        return "Makanan Dihidangkan";
+    }
+}
---
+++ src/Minuman.java
@@ -0,0 +1,6 @@
+public class Minuman extends Hidangan{
+    public String disantap(){
+        return this.getnamaHidangan() + "Diminum";
+    }
+
+}
